fbc4f32bf138edd5b3ee1e389a9c958566647c0a
lonehand <970106874@qq.com>
2019-11-25 14:54:39 +0800

    Update fingerprint.js

--- js/fingerprint.js
+++ js/fingerprint.js
@@ -15,6 +15,26 @@
 * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
 * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
+function myBrowser(){
+    var userAgent = navigator.userAgent; //取得浏览器的userAgent字符串
+    var isOpera = userAgent.indexOf("Opera") > -1;
+    if (isOpera) {
+        return "Opera"
+    }; //判断是否Opera浏览器
+    if (userAgent.indexOf("Firefox") > -1) {
+        return "FF";
+    } //判断是否Firefox浏览器
+    if (userAgent.indexOf("Chrome") > -1){
+  return "Chrome";
+ }
+    if (userAgent.indexOf("Safari") > -1) {
+        return "Safari";
+    } //判断是否Safari浏览器
+    if (userAgent.indexOf("compatible") > -1 && userAgent.indexOf("MSIE") > -1 && !isOpera) {
+        return "IE";
+    }; //判断是否IE浏览器
+    return "Unknown";
+}
 
 ;(function (name, context, definition) {
   if (typeof module !== 'undefined' && module.exports) { module.exports = definition(); }
@@ -73,7 +93,7 @@
   Fingerprint.prototype = {
     get: function(){
       var keys = [];
-      keys.push(navigator.userAgent);
+      keys.push(myBrowser());
       //if (navigator.userAgent.match(/Device\/\(.*\)/) !== null) {
 	 //keys.push(navigator.userAgent.match(/Device\/\(.*\)/)[0]);
       //} else {
