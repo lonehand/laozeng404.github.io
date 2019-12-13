64f11b2102b9ca669d0e7045543fe1cdb3a1d012
lonehand <970106874@qq.com>
2019-12-13 18:19:14 +0800

    Update and rename Fingerprint3.js to fingerprint3.js

--- js/fingerprint3.js
+++ js/fingerprint3.js
@@ -392,8 +392,30 @@
       done(fingerprint)
     }
   }
+  
+  function myBrowser(){
+    var userAgent = navigator.userAgent; //取得浏览器的userAgent字符串
+    var isOpera = userAgent.indexOf("Opera") > -1;
+    if (isOpera) {
+        return "Opera"
+    }; //判断是否Opera浏览器
+    if (userAgent.indexOf("Firefox") > -1) {
+        return "FF";
+    } //判断是否Firefox浏览器
+    if (userAgent.indexOf("Chrome") > -1){
+  return "Chrome";
+ }
+    if (userAgent.indexOf("Safari") > -1) {
+        return "Safari";
+    } //判断是否Safari浏览器
+    if (userAgent.indexOf("compatible") > -1 && userAgent.indexOf("MSIE") > -1 && !isOpera) {
+        return "IE";
+    }; //判断是否IE浏览器
+    return "Unknown";
+}
+  
   var UserAgent = function (done) {
-    done(navigator.userAgent)
+    done(myBrowser())
   }
   var webdriver = function (done, options) {
     done(navigator.webdriver == null ? options.NOT_AVAILABLE : navigator.webdriver)
